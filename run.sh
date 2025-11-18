ee38575952930075821aa1b25cc5f9da9d9c1dc1
Fraktal-PM3 <pm3@pm3fraktal.se>
2025-11-18 14:12:19 +0000

    Added external dns automation

--- run.sh
+++ run.sh
@@ -18,5 +18,8 @@ export KUBECONFIG=/home/hedlund01/bevel-fixes/build/config
 echo "Validatin network yaml"
 ajv validate -s /home/hedlund01/bevel-fixes/platforms/network-schema.json -d /home/hedlund01/bevel-fixes/build/network.yaml 
 
+echo "Setting up External DNS credentials..."
+bash /home/hedlund01/bevel-fixes/setup-external-dns.sh
+
 echo "Running the playbook..."
 exec ansible-playbook -vv /home/hedlund01/bevel-fixes/platforms/shared/configuration/site.yaml --inventory-file=/home/hedlund01/bevel-fixes/platforms/shared/inventory/ -e "@/home/hedlund01/bevel-fixes/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
